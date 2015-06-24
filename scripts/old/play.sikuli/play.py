006dde18cd82bfd9c3a07046b652d4b6c1e14326
vagrant <vagrant@precise64.(none)>
2015-06-24 15:40:58 +0000

    small updates, removed old files, sikuli install script added scripts/autostart/ --> ~/.config/autostart

---
+++ scripts/old/play.sikuli/play.py
@@ -0,0 +1,8 @@
+click("1434126065140.png")
+#click("1434126449289.png")
+
+rightClick("1434126234608.png")
+
+click("1434126394924.png")
+wait(2)
+type("echo 'Hello world'\n")
--- sikuli/open_flex.sikuli/open_flex.py
+++ sikuli/open_flex.sikuli/open_flex.py
@@ -1,3 +1,9 @@
 click("Createanewpr.png")
-type( "hello")
+wait(1)
+type( "hellooo")
 click("OK.png")
+if exists(Pattern("OK-2.png").similar(0.88)):
+    click(Pattern("OK-1.png").similar(0.86))
+else:
+    click(Pattern("Qpen.png").similar(0.80))
+    
