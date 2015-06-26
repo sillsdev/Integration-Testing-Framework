588ee38e4b8737d2e45ee4ed7c4ad484dc09186b
vagrant <vagrant@precise64.(none)>
2015-06-26 09:57:48 -0500

    change waiting wrappers so they don't exit on error

--- sikuli/examples/waiting_wrappers.sikuli/waiting_wrappers.py
+++ sikuli/examples/waiting_wrappers.sikuli/waiting_wrappers.py
@@ -6,18 +6,32 @@ time is specified.
 
 from sikuli import *
 
-def Click(thing, time=1):
-    click(thing)
+def Click(thing, debug=0, time=1):
+    try:
+        click(thing)
+    except FindFailed, ff:
+        if debug > 0:
+            popup(ff.message)
+            exit(1)
     wait(time)
 
-def DoubleClick(thing, time=1):
-    doubleClick(thing)
+def DoubleClick(thing, debug=0, time=1):
+    try:
+        doubleClick(thing)
+    except FindFailed, ff:
+        if debug > 0:
+            popup(ff.message)
+            exit(1)
     wait(time)
 
-def Type(text, time=1):
+def Type(text time=1):
     type(text)
     wait(time)
 
-def Find(thing, time=1):
-    find(thing)
-    wait(time)
+def Find(thing, debug=0, time=1):
+    try:
+        find(thing)
+    except FindFailed, ff:
+        if debug > 0:
+            popup(ff.message)
+            exit(1)
