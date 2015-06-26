bd6ae6cd81eb8d446e57cc25410846087075e078
vagrant <vagrant@precise64.(none)>
2015-06-26 11:57:28 -0500

    help_about script works; also fixed wrapper methods

--- sikuli/examples/help_about.sikuli/help_about.py
+++ sikuli/examples/help_about.sikuli/help_about.py
@@ -1,14 +1,26 @@
 from flex_regions import *
 from waiting_wrappers import *
-    
 
-TOOLBARS.Click("Help.png")
-Click("AboutLanguag.png")
-region = Region(2,2,1232,943)
-if region.exists("7AboutSILFle.png") is None:
-    f = open("error_log", "a")
+def write_log(line):
+    f = open("/vagrant/error_log", "a+")
     try:
-        f.write("about screen didn't pop up\n")
+        f.write(line + "\n")
     finally:
         f.close()
+   
+try:
+    TOOLBARS.Click("Help.png")
+except FindFailed:
+    write_log("Help button not found")
+    print "1"
+
+try:
+    Click("AboutLanguag.png")
+except FindFailed:
+    write_log("About Language Explorer button not found")
+    print "2"
+    
+region = Region(2,2,1232,943)
+if region.exists("7AboutSILFle.png") is None:
+    write_log("about screen didn't pop up")
 
