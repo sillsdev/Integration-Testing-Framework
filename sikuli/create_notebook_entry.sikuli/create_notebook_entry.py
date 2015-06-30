8e2ab2160800520168a5ea8215008bbcaed59b39
Orynae <jess.n.dawson@gmail.com>
2015-06-30 16:23:53 -0500

    Comments added + now quits on certain failures

--- sikuli/create_notebook_entry.sikuli/create_notebook_entry.py
+++ sikuli/create_notebook_entry.sikuli/create_notebook_entry.py
@@ -6,16 +6,25 @@ from test_helper import TestHelper
 from flex_regions import *
 from Regionplus import *
 
+# Set up helper
 helper = TestHelper("create_notebook_entry")
 set_flex_helper(helper)
 
-helper.Click("Notebook5.png", "Couldn't find 'Notebook' button")
-LEFT_SIDEBAR.Click("RecordEdit.png", "Couldn't find 'Record Edit' button")
-TOOLBARS.Click(Pattern("1435614864882.png").similar(0.96), "Couldn't find 'Create new record' button")
+# Opening
+##############
+helper.Click("Notebook5.png", "Couldn't find 'Notebook' button", quit=True)
+LEFT_SIDEBAR.Click("RecordEdit.png", "Couldn't find 'Record Edit' button", quit=True)
+TOOLBARS.Click(Pattern("1435614864882.png").similar(0.96), "Couldn't find 'Create new record' button", quit=True)
 
+# Goal
+##############
 helper.Type("hello world" + Key.ENTER)
 
 Regionplus(helper, Region(147,101,537,692)).Exists("helloworld.png",
         "Cannot find new 'hello world' entry")
 
 helper.write_success()
+
+# Closing
+##############
+# (None)
