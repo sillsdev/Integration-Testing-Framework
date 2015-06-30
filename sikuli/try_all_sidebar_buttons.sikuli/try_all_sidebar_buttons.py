908ff1a98675de0cae0d477b118838646319f6b0
Orynae <jess.n.dawson@gmail.com>
2015-06-30 14:28:47 -0500

    Now using TestHelper isntead of Logger

--- sikuli/try_all_sidebar_buttons.sikuli/try_all_sidebar_buttons.py
+++ sikuli/try_all_sidebar_buttons.sikuli/try_all_sidebar_buttons.py
@@ -2,8 +2,8 @@ import sys
 sys.path.insert(0, '/home/vagrant/linux_setup/sikuli/examples')
 
 from sikuli import *
-import flex_regions
-from logger import Logger
+from flex_regions import *
+from test_helper import TestHelper
 
 """
 Click through all of the buttons in the left sidebar.
@@ -14,54 +14,49 @@ If an information box pops up, close it.
 Return to first option when done.
 """
 
-changed = False
+helper = TestHelper("try_all_side_bar_buttons")
+set_flex_helper(helper)
 
 def stop_observer(event):
     global changed
-    if Region(148,139,544,545).exists("information_popup.png") is not None:
-        type(Key.ENTER)
-    else:
-        event.region.stopObserver()
-        changed = True
+    changed = True
+    event.region.stopObserver()
+    if Region(148,139,544,545).exists("information_popup.png"):
+        helper.Type(Key.ENTER, time=5)
     
+# Setup to the correct initial screen
+helper.Click("Lexicon.png", "Couldn't find 'Lexicon' button")
+first_region = Region(21,127,115,15)
+click(first_region.getCenter())
 
-def try_all_sidebar_buttons():
-    log = Logger("try_all_side_bar_buttons")
+# Set initial region to click
+region = first_region.offset(Location(0, 18))
+count = 0
+
+# keep clicking while the list still has items 
+while not region.exists(Pattern("blank_space.png").similar(0.99)):
     
-    first_region = Region(21,127,115,15)
+    global changed
+    changed = False
 
-    # Set initial region to click
-    region = first_region.offset(Location(0, 18))
-    count = 0
-    
-    # keep clicking while the list still has items 
-    while region.exists(Pattern("blank_space.png").similar(0.99)) == None:
-        
-        global changed
-        changed = False
+    # keep count and make sure we don't go too far
+    count += 1
+    if count > 9:
+        helper.write_fail("Script malfunction: went too far down the left column")
+        break;
 
-        # keep count and make sure we don't go too far
-        count += 1
-        if count > 9:
-            log.write_fail("Script malfunction: went too far down the left column")
-            break;
+    # Click in the middle of the region and react to change
+    MID_TOOLBAR.onChange(stop_observer)
+    click(region.getCenter())
+    MID_TOOLBAR.observe(5)
+    # Check if the observer stopped as intended
+    if not changed:
+        helper.write_fail("No change when clicking item " + str(count))
 
-        # Click in the middle of the region and react to change
-        flex_regions.MID_TOOLBAR.onChange(stop_observer)
-        click(region.getCenter())
-        flex_regions.MID_TOOLBAR.observe(5)
-        # Check if the observer stopped as intended
-        if not changed:
-            log.write_fail("No change when clicking item " + str(count))
+    # Move region down by 18 pixels
+    region = region.offset(Location(0, 18))
 
-        # Move region down by 18 pixels
-        region = region.offset(Location(0, 18))
+# go back to first screen
+click(first_region.getCenter())
 
-    # go back to first screen
-    click(first_region.getCenter())
-
-    if not log.has_fail():
-        log.write("Success")
-
-
-try_all_sidebar_buttons()
+helper.write_success()
