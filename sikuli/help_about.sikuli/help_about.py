ab6d2bcce44b776c9c7cd2a9d9eefd924cd8e265
Orynae <jess.n.dawson@gmail.com>
2015-06-30 14:04:12 -0500

    Now using TestHelper instead of Logger

--- sikuli/help_about.sikuli/help_about.py
+++ sikuli/help_about.sikuli/help_about.py
@@ -2,28 +2,16 @@ import sys
 sys.path.insert(0, '/home/vagrant/linux_setup/sikuli/examples')
 
 from flex_regions import *
-from waiting_wrappers import *
-from logger import Logger
+from test_helper import TestHelper
 
-log = Logger("help_about")
-try:
-    TOOLBARS.Click("Help.png")
-except FindFailed:
-    log.write_fail("Help button not found")
+helper = TestHelper("help_about")
+set_flex_helper(helper)
 
-try:
-    Click("AboutLanguag.png")
-except FindFailed:
-    log.write_fail("About Language Explorer button not found")
+TOOLBARS.Click("Help.png", "Help button not found")
+helper.Click("AboutLanguag.png", "About Language Explorer button not found")
     
-region = Region(2,2,1232,943)
-if region.exists("7AboutSILFle.png") is None:
-    log.write_fail("`About` screen didn't pop up")
-try:
-    Click("OK.png")
-except FindFailed:
-    log.write_fail("`About` screen doesn't contain `OK`")
+helper.Exists("7AboutSILFle.png", "`About` screen didn't pop up")
+helper.Click("OK.png", "`About` screen doesn't contain `OK`")
 
-if not log.has_fail():
-    log.write("Success")
+helper.write_success()
 
