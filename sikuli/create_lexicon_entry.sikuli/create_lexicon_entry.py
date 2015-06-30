0b3eeef2c3eac07ac89107c8ac789c690312c648
Orynae <jess.n.dawson@gmail.com>
2015-06-30 16:22:40 -0500

    Adjusted one of the images

--- sikuli/create_lexicon_entry.sikuli/create_lexicon_entry.py
+++ sikuli/create_lexicon_entry.sikuli/create_lexicon_entry.py
@@ -10,7 +10,7 @@ helper = TestHelper("create_lexicon_entry")
 set_flex_helper(helper)
 
 helper.Click("Lexicon.png", "Couldn't find 'Lexicon' button")
-LEFT_SIDEBAR.Click("EHLexiconEdi.png", "Couldn't find 'Lexicon Edit' button")
+LEFT_SIDEBAR.Click("LexiconEdit.png", "Couldn't find 'Lexicon Edit' button")
 MID_TOOLBAR.Click("Headword.png", "Couldn't find Headword button")
 TOOLBARS.Click("1435675185765.png", "Couldn't find 'Create new lexical entry' button")
 
