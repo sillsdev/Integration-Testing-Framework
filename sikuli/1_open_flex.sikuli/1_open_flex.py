58cc10b6c6399c2862ad51cb68474c941ca75c37
Orynae <jess.n.dawson@gmail.com>
2015-07-02 11:09:36 -0500

    open_flex refactor
    1_open_flex functionality moved to open_new_project() function
    in open_new_project in the example folder,
    so it can be used elsewhere (i.e. in restart_flex).
    1_open_flex just calls that function.

--- sikuli/1_open_flex.sikuli/1_open_flex.py
+++ sikuli/1_open_flex.sikuli/1_open_flex.py
@@ -1,30 +1,6 @@
 import sys
 sys.path.insert(0, '/home/vagrant/linux_setup/sikuli/examples')
-from test_helper import TestHelper
+from open_new_project import open_new_project
 
-helper = TestHelper("open_flex")
-
-helper.Click("Createanewpr.png", "Cannot find `Create a new project`")
-type("hello")
-helper.Click("OK.png", "Cannot find `OK`")
-if exists(Pattern("OK-2.png").similar(0.88)):
-    helper.Click(Pattern("OK-1.png").similar(0.86), "Cannot find `OK`")
-else:
-    helper.Click(Pattern("Qpen.png").similar(0.80), "Cannot find `Open`")
-    
-def open_handler(event):
-    helper.write("Successfully opened flex.")
-    #event.region.stopObserver()
-
-def green_handler(event):
-    helper.write_fail("An error has occurred (green)")
-    event.region.stopObserver()
-    
-onAppear("1435347136957.png", open_handler)
-onAppear(Pattern("Anerrorhasoc.png").similar(0.80), green_handler)
-observe(40)
-
-if helper.has_fail():
-    helper.write_fail("Failed to open")
-
+open_new_project()
 
