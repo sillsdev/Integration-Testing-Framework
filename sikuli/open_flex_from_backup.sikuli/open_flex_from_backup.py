7419613bc7848c0f0cd1cb267cf782f3956404a7
Orynae <jess.n.dawson@gmail.com>
2015-07-08 09:54:52 -0500

    Added script to open a flex backup file.
    Can open it either from the first flex screen (the one with all the different options to open stuff)
    or from within a project. You gotta tell it which one is the case, and give an absolute
    path to the backup.

---
+++ sikuli/open_flex_from_backup.sikuli/open_flex_from_backup.py
@@ -0,0 +1,49 @@
+from sikuli import *
+import sys
+sys.path.insert(0, '/home/vagrant/linux_setup/sikuli/examples')
+from test_helper import TestHelper
+from Regionplus import Regionplus
+
+# In a function so we can it to run from another test.
+
+def close_box():
+    helper.Click("Cancel.png", "Cancel button not found", restart=True)
+
+# path: should be the absolute path to the .fwbackup file.
+# flex_is_open: True if Flex is already open, and we're opening
+# the project via File > Project Management > Restore.
+# False if we're just on Flex's opening screen.
+def open_backup(path, flex_is_open):
+    # Opening: Get to the 'Restore a Project' screen
+    ################
+    helper = TestHelper("open_flex_from_backup")
+
+    if flex_is_open:
+        helper.Click("File.png", "Couldn't find 'File'")
+        helper.Click("ProjectManag.png",
+            "Couldn't find 'Project Management' in File menu")
+        helper.Click("RestoreaProj.png", "Couldn't find 'Restore a Project...' option")
+    else:
+        # Not sure if this line is needed...
+        #subprocess.Popen("fieldworks-flex &")
+        helper.Click(Pattern("Restoreaproj.png").similar(0.89), 
+            "Couldn't find 'Restore a project from a backup file...'")
+        
+    # Goal: Find the backup
+    #################
+    if not helper.Click(Pattern("Anotherlocat.png").similar(0.80).targetOffset(-48,1), "Couldn't find 'Another location' option", give_up=False):
+        close_box()
+    if not helper.Click(Pattern("Browse.png").similar(0.90), "Couldn't find 'Browse...' button", give_up=False):
+        close_box()
+    helper.Click("1436284725146.png", "Couldn't find button to type a file name", restart=True)
+    paste(path)
+    helper.Type(Key.ENTER)
+   
+    helper.Click(Pattern("OK.png").similar(0.91), "Couldn't click 'OK'", restart=True)
+    
+    if helper.Exists(Pattern("7ReplaceExls.png").similar(0.88), "Success"):
+        helper.Click(Pattern("Yes.png").similar(0.95), "Couldn't click 'Yes'", time=10, restart=True)
+        helper.write_success()
+    else:
+        wait(20)
+        
