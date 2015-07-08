1ff7ae3eaba7710aed504003b3e2b85f2193bdc8
vagrant <rrmhearts@gmail.com>
2015-07-08 15:50:07 +0000

    backed up stuff before pull... things changed.. won't run until I pull but want compare differences in these files

---
+++ sikuli/old/1_open_flex_existing.sikuli/1_open_flex_existing.py
@@ -0,0 +1,14 @@
+# Open an existing project entitled "hello"
+# Used to restart flex if it is broken...
+# Need to re-write more robust
+click("Openaproject.png")
+wait(1)
+click("0OpenProject.png")
+wait(1)
+
+click("ectfromacoll.png")
+wait(10)
+
+#click("coueaouewfhp.png")
+
+
