c107a558d7e159a5955f609cbd735a4eaca13cbc
Orynae <jess.n.dawson@gmail.com>
2015-07-09 11:12:04 -0500

    Finished script to compare word/text/dictionary screenshots
    Includes a script to compare several things on 3 projects
    after loading them from backup.

--- sikuli/compare_screenshots_from_backups.sikuli/compare_screenshots_from_backups.py
+++ sikuli/compare_screenshots_from_backups.sikuli/compare_screenshots_from_backups.py
@@ -9,20 +9,22 @@ folder = "/home/vagrant/linux_setup/sikuli/examples/images_for_comparison/"
 
 # Open Tagbanwa
 open_flex_from_backup.open_backup("/vagrant/flex projects/Tagbanwa, Calamian 2015-07-07 1037 for testing purposes.fwbackup", True)
-# TODO: see if dictionary view has changed
+check_change.check_dictionary(folder + "Tagbanwa - dictionary.png")
 check_change.check_word("dalik", folder + "Tagbanwa - dalik.png") # IXTERMINATE
 check_change.check_word("bugnawan", folder + "Tagbanwa - bugnawan.png")
 
 # Open Kamasau
 open_flex_from_backup.open_backup("/vagrant/flex projects/Kamasau 2015-07-07 1036 for testing purposes.fwbackup", True)
-# TODO: see if dictionary view has changed
-check_change.word("chiraq", folder + "Kamasau - chiraq.png") # like the French president in like the 2000s
+check_change.check_dictionary(folder + "Kamasau - dictionary.png")
+check_change.check_word("chiraq", folder + "Kamasau - chiraq.png") # like the French president in like the 2000s
 check_change.check_word("gre", folder + "Kamasau - gre.png")
 
 # Open Ayta Mag-Anchi
 open_flex_from_backup.open_backup("/vagrant/flex projects/Ayta Mag-Anchi2 2015-07-07 1035 for testing purposes.fwbackup", True)
 check_change.check_text("kulot2.ptx", folder + "Ayta - kulot2.ptx.png")
 
+# Restart flex to hello project, closing the 3 windows
+# we just opened + whatever was open before
 helper.restart_flex()
 
 
