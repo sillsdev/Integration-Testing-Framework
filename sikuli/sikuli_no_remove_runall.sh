7d3819bfbd0d9309ed58db058225a5b0369ad1e5
rrmhearts <rrmhearts@gmail.com>
2015-07-21 20:52:30 +0000

    updated potential sikuli runall scripts to include a wait one. renamed 'norm' to no_remove

--- sikuli/sikuli_no_remove_runall.sh
+++ sikuli/sikuli_no_remove_runall.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-#sleep 30s
-rm -Rf /home/vagrant/.local/share/fieldworks/Projects/hel*
+#sleep 6m
+#rm -Rf /home/vagrant/.local/share/fieldworks/Projects/hel*
 for D in `find /home/vagrant/linux_setup/sikuli/*.sikuli -type d`
 do
 	ps cax | grep mono > /dev/null
 	if [ $? -eq 0 ]; then
 	  echo Running $D ...
-	  echo Running $D ... >> /vagrant/error_log
           sikuli $D
           rm /home/vagrant/*.png > /dev/null
 	else
 	  echo "FLEX is not running."
-	  echo "FLEX is not running." >> /vagrant/error_log
 	  exit #break
 	fi
 done
