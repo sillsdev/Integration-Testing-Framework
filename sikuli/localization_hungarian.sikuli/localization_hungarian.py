6876fe3665b6be4d374f563758cf4deeee3f801b
mr-rogers <krmail@gmail.com>
2015-11-24 09:55:21 -0700

    Update localization tests images and rename localization folders
    Images and regions needed to be updated for FLEX 8.2.
    Only a couple images needed to be changed. A switch was added in the
    generic test to capture images for each language to compare manually.
    An enlish localization was added as a sanity check and as a reset
    capability in the event that a non-English localization doesn't finish.
    The folders were changed in preparation for the xmlrunner capability
    so tests would not have the names "test_test_*localization".

---
+++ sikuli/localization_hungarian.sikuli/localization_hungarian.py
@@ -0,0 +1,5 @@
+from sikuli import *
+from a_setup import *
+import localizations_test
+
+localizations_test.run_test(5)
--- sikuli/test_turkish_localization.sikuli/test_turkish_localization.py
+++
@@ -1,5 +0,0 @@
-from sikuli import *
-from a_setup import *
-import test_localizations
-
-test_localizations.run_test(9)
