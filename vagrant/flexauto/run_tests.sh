cb03151a7759eea5c76a45557496e127accc7130
RogersK <rogersk@dallas.sil.org>
2016-05-25 11:06:05 -0500

    Apply miscellaneous fixes
    Linux Versions were too strict. Some OS may add hidden characters to the
    logFolder.txt. The error_log was alongside the other log files, but isn't
     really a user file. The UserLog file did not need to be created
    automatically. The Similarity header went missing. Starting flex  does not
    need to be in run_tests.sh.
    OS versions were limited to Major and Minor attributes. Checking was
    relaxed for logFolder contents. UserLog creation was suppressed until
    expressly used. The Similarity header was added in the CSS and test_helper.
    Starting flex commands were removed from run_tests.sh.

--- vagrant/flexauto/run_tests.sh
+++ vagrant/flexauto/run_tests.sh
@@ -4,13 +4,6 @@ export DISPLAY=:0.0
 ### Change screen resolution
 xrandr --output VGA-0 --mode 1024x768
 
-### FieldWorks Sharing
-#/usr/lib/fieldworks/ShareFwProjects
-
-### Start Flex
-sudo rm -Rf /home/vagrant/.local/share/fieldworks/Projects/*
-fieldworks-flex &
-
 ### Sikuli XScripts
 runsikulix -r /home/vagrant/Integration-Testing-Framework/general_tests/_sikuli_runall.sikuli
 
